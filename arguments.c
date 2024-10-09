0be3aa0f5372bf00042c4f270237bd9fce35fd0b
Jack <jackjibb@gmail.com>
2024-10-09 16:31:19 +0100

    old ass c wont let me define i in the loop

--- arguments.c
+++ arguments.c
@@ -3,11 +3,12 @@
 
 int main(int argc, char **argv) {
   int n;
+  int i = 0;
   int sum = 0;
   n = atoi(argv[1]);
   int squares[n]; // can this work?
 
-  for (int i = 1; i < n; i++) {
+  for (i = 1; i < n; i++) {
     squares[i] = i * i;
     printf("%d %d\n", i, squares[i]);
     sum += squares[i];
